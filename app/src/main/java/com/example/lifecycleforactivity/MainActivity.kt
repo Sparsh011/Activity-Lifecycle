9e161bceb482052e6874e59271ddefda86512f5f
Sparsh <sparsh.chadha46@gmail.com>
2023-01-17 13:21:36 +0530

    onStart vs onCreate checked

--- app/src/main/java/com/example/lifecycleforactivity/MainActivity.kt
+++ app/src/main/java/com/example/lifecycleforactivity/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
 
 //        Q. Why shouldn't we write logical part related to views in onStart?
-//        Ans. All the logic part should be written in onCreate and not in onStart because, let's say we want to show a toast or do something whenever the activity is launched for the first time and if we write that code in onStart, then if we navigate to any other activity and come back to the original activity, then that toast will be shown again and we don't want this because activity IS NOT recreated at that moment.
+//        Ans. All the logic part should be written in onCreate and not in onStart because, let's say we want to show a toast or do something whenever the activity is launched for the first time and if we write that code in onStart, then if we navigate to any other activity and come back to the original activity, then that toast will be shown again and we don't want this because activity IS NOT recreated at that moment. Also, all the processing will be done again and again in this scenario
 
 //        Follow up ques. But if the device is rotated, then onCreate will be called again, then the toast will be shown again, how to overcome this problem?
 //        Ans. By storing whether the toast was shown or not in bundle or a global variable. Bundle's data lives as long as the application lives.
